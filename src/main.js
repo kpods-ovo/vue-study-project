a60ca87d8ac40be164aabbd2de4c32fac2fbc38f
kpods <422710684@qq.com>
2020-10-15 17:30:57 +0800

    add vue-router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 // 入口文件
 import Vue from 'vue';
 
+// 导入路由的包且安装
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
 // 导入App.vue组件
 import app from './App.vue';
+
+// 导入router.js文件
+import router from './router.js'
 
 // 引入mint-ui组件库的相关组件
 import {Header} from 'mint-ui';
 
 // 引入mui的相关文件
 import './lib/mui/css/mui.css';
+import './lib/mui/css/icons-extra.css'
 
 Vue.component(Header.name,Header);
 
@@ -22,6 +30,7 @@ var vm = new Vue({
     },
     render:function(c){
         return c(app)
-    }
+    },
+    router
 })
 
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router';
+
+var router = new VueRouter({
+    routes: [
+        // { path: '/', redirect: '/login' },
+        // { path: '/home', component: home },
+        // { path: '/member', component: member },
+        // { path: '/shopcar', component: shopcar },
+        // { path: '/search', component: search }
+    ],
+    linkActiveClass:'mui-active'
+})
+
+export default router;
